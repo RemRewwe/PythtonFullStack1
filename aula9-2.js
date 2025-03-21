1d7fb36280156c21dc82a4870db8c11833390b65
RemRewwe <arturferreirapaixao8@gmail.com>
2025-03-21 00:16:09 -0300

    Update aula9-2.js

--- aula9-2.js
+++ aula9-2.js
@@ -1,6 +1,10 @@
-let num = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-let soma = 1 + 2 + 3 + 4 + 5 
-console.log(soma)
+function calcular() {
+    let num = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    let soma = 1 + 2 + 3 + 4 + 5;
+    console.log(soma);
+}
+
+calcular();
 
 let alfa = [h, f, a, i, p, o, s, c, b, d]
 console.log(alfa[2, 8, 7, 9, 1, 0, 3, 6, 5, 4])
